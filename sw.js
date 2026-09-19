3497fa085c6070bfcf2d217b0a010d76a849281f
preetpatil214@gmail.com <preetpatil214@gmail.com>
2026-09-19 14:43:49 +0530

    v10

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change this version whenever the cached app shell or assets change.
-const CACHE_VERSION = 'nudge-v8';
+const CACHE_VERSION = 'nudge-v9';
 const APP_SHELL = [
   '/',
   '/index.html',
